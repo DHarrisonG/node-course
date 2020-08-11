9204d3cca031e14421cf0c661f8b329163d14a82
github username <dougharrisong@gmail.com>
2020-08-11 16:45:04 -0500

    Finished Chapter 27: Callback Abstraction

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 const weatherStackUrl = 'http://api.weatherstack.com/current?access_key=2fada2a74c18990f348ff523344564bd&query=37.8267,-122.4233&units=f'
 const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/los%2angeles.json?types=address&proximity=-122.39738575285674,37.7925147111369453&access_token=pk.eyJ1IjoiZ2ltbWlldXJhcGlzIiwiYSI6ImNrOThtZDB1eTE3b2gzbGxjN3Fycmc3M2oifQ.esbM1KQ_kH0lQa61pkMM-Q&limit=1"
@@ -14,16 +15,23 @@ const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/los%2angele
 //     }
 // })
 
-request({url: mapBoxUrl, json: true}, (error, response) => {
-    if (error) {
-        console.log("Turn the internet on doofus")
-    } else if (response.body.features.length === 0) {
-        console.log("Unable to find location")
-    } else {
-        const lat = response.body.features[0].center[1]
-        const lon = response.body.features[0].center[0]
-        console.log(`Damn, ${lat} ${lon}, look at all dem coordinates... `)
+// request({url: mapBoxUrl, json: true}, (error, response) => {
+//     if (error) {
+//         console.log("Turn the internet on doofus")
+//     } else if (response.body.features.length === 0) {
+//         console.log("Unable to find location")
+//     } else {
+//         const lat = response.body.features[0].center[1]
+//         const lon = response.body.features[0].center[0]
+//         console.log(`Damn, ${lat} ${lon}, look at all dem coordinates... `)
 
-    }
+//     }
 
+// })
+
+
+
+geocode('New York', (error, data) => {
+    console.log('Error: ', error)
+    console.log('Data: ', data)
 })
