83d0f8e5a63d7594bfcce73dbb3222dcad3ed23d
github username <dougharrisong@gmail.com>
2020-08-12 12:20:29 -0500

    Finished chap 40 destructuring and prop shorthand chall

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,13 @@ const request = require('request')
 forecast = (lat, lon, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=2fada2a74c18990f348ff523344564bd&query=${lon},${lat}&units=f`
 
-    request({url: url, json: true}, (error, response) => {
+    request({url, json: true}, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out. It feel's like ${response.body.current.feelslike} out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feel's like ${body.current.feelslike} out.`)
         }
     })
 }
