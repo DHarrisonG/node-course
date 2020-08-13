59bd9c263a2a1fc55d6eb7f6b5e3b23f8a7d5433
github username <dougharrisong@gmail.com>
2020-08-13 11:53:16 -0500

    Finished chap 49: advanded temps

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirPath))
 
 app.get('', (req, res) => {
@@ -23,7 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        msg: "This is the help message"
+        msg: "This is the help message",
+        title: 'Help',
+        name: 'Lacy Pup'
     })
 })
 
