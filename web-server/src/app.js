97547ffe78e79abf8982a472e137c82f59287edc
github username <dougharrisong@gmail.com>
2020-08-13 09:55:02 -0500

    Finished Chatp 44: Serving HTML and JSON

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,27 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Homepage</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page')
+    res.send([{
+        name: 'Doug',
+        age: 32
+    }, {
+        name: 'Lacy'
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.send('<h1>About page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page')
+    res.send({
+        location: 'Pittsburgh',
+        forcast: 88
+    })
 })
 
 app.listen(3000, () => {
