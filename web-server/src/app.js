0274678c49c27b07512d49451dcc6f69af618204
github username <dougharrisong@gmail.com>
2020-08-13 11:21:26 -0500

    Finished chap 47: Dynamic Pages with Temps

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,8 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Lacy Pup'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Lacy Pup'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        msg: "This is the help message"
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
