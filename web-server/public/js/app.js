aed24b883c9b89cbe452e873f5a483981260510d
DHarrisonG <dougharrisong@gmail.com>
2020-08-19 13:19:58 -0500

    finished Chap 59: Wiring User Interface

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,24 +1,29 @@
 console.log('Client side javascript is loaded')
 
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#msg-1')
+const messageTwo = document.querySelector('#msg-2')
+
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-
+    
     const location = search.value
     console.log(location)
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
 
     fetch(`http://localhost:3000/weather?address=${location}`)
     .then(res => res.json())
     .then(data => {
         if(data.error){
-            console.log(data.error)
+              messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
         }
     })
     
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?types=address&proximity=-122.39738575285674,37.7925147111369453&access_token=pk.eyJ1IjoiZ2ltbWlldXJhcGlzIiwiYSI6ImNrOThtZDB1eTE3b2gzbGxjN3Fycmc3M2oifQ.esbM1KQ_kH0lQa61pkMM-Q&limit=1`
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiZ2ltbWlldXJhcGlzIiwiYSI6ImNrOThtZDB1eTE3b2gzbGxjN3Fycmc3M2oifQ.esbM1KQ_kH0lQa61pkMM-Q&limit=1`
 
     request({url: url, json: true }, (error, { body }) => {
 
