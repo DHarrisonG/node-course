6ec8bbc663c201bf1de42cece7b2b372e7ac9403
DHarrisonG <dougharrisong@gmail.com>
2020-08-18 19:42:34 -0500

    Finished Chap 57: HTTP Requests with Fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log('Client side javascript is loaded')
+
+fetch('http://localhost:3000/weather?address=pittsburgh')
+.then(res => res.json())
+.then(data => {
+    if(data.error){
+        console.log(data.error)
+    } else {
+        console.log(data.location)
+        console.log(data.forecast)
+    }
+})
