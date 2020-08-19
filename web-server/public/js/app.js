e76adca79d51e10a92dda1bbcbe94377f8adb521
DHarrisonG <dougharrisong@gmail.com>
2020-08-19 12:10:08 -0500

    Finished Chap 58: Creating Search Form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,27 @@
 console.log('Client side javascript is loaded')
 
-fetch('http://localhost:3000/weather?address=pittsburgh')
-.then(res => res.json())
-.then(data => {
-    if(data.error){
-        console.log(data.error)
-    } else {
-        console.log(data.location)
-        console.log(data.forecast)
-    }
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    console.log(location)
+
+    fetch(`http://localhost:3000/weather?address=${location}`)
+    .then(res => res.json())
+    .then(data => {
+        if(data.error){
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+    
+
+
 })
