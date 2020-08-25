45278938d25c07abd64153e0bb943dd10f442fb8
DHarrisonG <dougharrisong@gmail.com>
2020-08-25 16:04:13 -0500

    recommiting entire course

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -9,7 +9,7 @@ forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feel's like ${body.current.feelslike} out.`)
+            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out with a humidity of ${body.current.humidity}. It feel's like ${body.current.feelslike} out.`)
         }
     })
 }
