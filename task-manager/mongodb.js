2ffbff2af6475dd7b7d4339973a8ea35d6326574
DHarrisonG <dougharrisong@gmail.com>
2020-08-25 19:44:12 -0500

    Finished Chap 76. Inserting Documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -11,10 +11,50 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database')
     }
 
-   const db = client.db(databaseName)
+    const db = client.db(databaseName)
 
-   db.collection('users').insertOne({
-       name: 'LacyPupper',
-       age: 8
-   })
+    // db.collection('users').insertOne({
+    //     name: 'LacyPupper',
+    //     age: 8
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Lacy',
+    //         age: 8
+    //     }, {
+    //         name: 'Nicole',
+    //         age: 29
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert users!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Walk the puppers',
+            completed: false
+        }, {
+            description: 'Pet the puppers',
+            completed: true
+        }, {
+            description: 'Feed the puppers',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            console.log('Unable to insert tasks!')
+        }
+        console.log(result.ops)
+    })
 })
