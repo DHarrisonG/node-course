64f504bcbc381fa79c580d7a19c6192ec3c87f1c
DHarrisonG <dougharrisong@gmail.com>
2020-08-26 12:57:59 -0500

    Finished Chap 79. Promises

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -31,12 +31,29 @@
 // 3. After 2 seconds are up, call the callback function with the sum
 // 4. Test your work!
 
-add = (a, b, callback) => {
+// add = (a, b, callback) => {
+//     setTimeout(() => {
+//         callback(a + b)
+//     }, 2000)
+// }
+
+// add(1, 4, (sum) => {
+//     console.log(sum) // Should print: 5
+// })
+
+// Chap 79 below 
+
+
+const doWorkCallback = (callback) => {
     setTimeout(() => {
-        callback(a + b)
+        callback(undefined, )
     }, 2000)
 }
 
-add(1, 4, (sum) => {
-    console.log(sum) // Should print: 5
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
 })
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -2,6 +2,7 @@
 
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -13,48 +14,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     name: 'LacyPupper',
-    //     age: 8
-    // }, (error, result) => {
+    // db.collection('users').findOne({ _id: new ObjectID("5f467ead9b43ce0b2a8e8bc5") }, (error, user) => {
     //     if (error) {
-    //         return console.log('Unable to insert user')
+    //         return console.log('User not found')
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(user)
     // })
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Lacy',
-    //         age: 8
-    //     }, {
-    //         name: 'Nicole',
-    //         age: 29
-    //     }
-    // ], (error, result) => {
+    // db.collection('users').find({ age: 27 }).toArray((error, users) => {
     //     if (error) {
-    //         return console.log('Unable to insert users!')
+    //         return console.log('User not found')
     //     }
 
-    //     console.log(result.ops)
+    //     console.log(users)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'Walk the puppers',
-            completed: false
-        }, {
-            description: 'Pet the puppers',
-            completed: true
-        }, {
-            description: 'Feed the puppers',
-            completed: true
-        }
-    ], (error, result) => {
-        if (error) {
-            console.log('Unable to insert tasks!')
-        }
-        console.log(result.ops)
+    db.collection('tasks').findOne({ _id: new mongodb.ObjectId("5f45af7e3db608f9043499b6")}, (error, task) => {
+        console.log(task)
+    })
+
+    db.collection('tasks').find( {completed: false}).toArray((error, tasks) => {
+        console.log(tasks)
     })
 })
