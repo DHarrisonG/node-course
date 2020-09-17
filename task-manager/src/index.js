42a82fbfbdf8ebb9ef1823eeb9d728cc01fb38b9
DHarrisonG <dougharrisong@gmail.com>
2020-09-17 11:01:08 -0500

    Chap 100. added delete functionality

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -105,7 +105,7 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
-app.patch('tasks:id', async (req, res) => {
+app.patch('/tasks/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -125,6 +125,34 @@ app.patch('tasks:id', async (req, res) => {
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e){
+        res.status(500).send()
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e){
+        res.status(500).send()
     }
 })
 
