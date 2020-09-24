44d4baa99ddfd544c9f955c21520e3e9e18a0c04
DHarrisonG <dougharrisong@gmail.com>
2020-09-24 16:26:06 -0500

    Finished chap 109, generating auth tokens... he's starting to lose me here...

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -31,7 +31,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     } catch (e) {
         res.status(400).send(e)
     }
@@ -40,11 +41,11 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
-        res.send(user)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     } catch (e) {
         res.status(400).send()
     }
-
 })
 
 router.patch('/users/:id', async (req, res) => {
