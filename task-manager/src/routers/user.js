c9e19c8b21e355fe8e4a60fbd6b7f5595e8c4dfd
DHarrisonG <dougharrisong@gmail.com>
2020-09-29 14:44:32 -0500

    logging out functionality

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -25,6 +25,17 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
 router.get('/users/me', auth,  async (req, res) => {
     res.send(req.user)
 })
